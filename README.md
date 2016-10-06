177353ac081a222ee2da53cdcd81c8f2d32fed95
Vina Compson <vv.compson@icloud.com>
2016-10-06 05:59:10 +0000

    Yelpcamp v5

--- README.md
+++ README.md
@@ -28,4 +28,20 @@
 * Add the comment new and create routes
 * Add the new comment form
 
+RESTFUL ROUTES  - good to have structure - to follow pattern - lots of app will follow that makes it reliable to interact to other app
 
+name    url             verb    desc
+====================================================
+INDEX   /dogs            GET    Display a list of all dog
+NEW     /dogs/new       GET     Display form to make a new dog
+CREATE  /dogs           POST    Add new dog to DB
+SHOW    /dogs/:id       GET     Shows info about one dog
+
+
+INDEX   /campgrounds
+NEW     /campgrounds/new
+CREATE  /campgrounds
+SHOW    /campgrounds/:id
+
+NEW     campgrounds/:id/comments/new    GET
+CREATE  camgorunds/:id/comments     POST
--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
     seedDB      = require("./seeds");
     
-mongoose.connect("mongodb://localhost/yelp_camp_v3");
+mongoose.connect("mongodb://localhost/yelp_camp_v4");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 seedDB();
@@ -21,7 +22,7 @@ app.get("/campgrounds", function(req, res){
        if(err){
            console.log(err);
        } else {
-          res.render("index",{campgrounds:allCampgrounds});
+          res.render("campgrounds/index",{campgrounds:allCampgrounds});
        }
     });
 });
@@ -46,7 +47,7 @@ app.post("/campgrounds", function(req, res){
 
 //NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-   res.render("new.ejs"); 
+   res.render("campgrounds/new"); 
 });
 
 // SHOW - shows more info about one campground
@@ -58,10 +59,53 @@ app.get("/campgrounds/:id", function(req, res){
         } else {
             console.log(foundCampground);
             //render show template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
 });
+
+
+//++++++++++++++++++++++
+//COMMENTS ROUTES
+//++++++++++++++++++++++
+
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    //find campground by ud
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new", {campground: campground});
+        }
+    });
+    
+    
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+   //create new comment
+   //connect new comment to campground
+   //redirect campground show page
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
